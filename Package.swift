a4360fef8df56883e395596a543d740ee20eff95
Tito <tito@channel.surf>
2024-08-30 09:06:33 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -6,7 +6,7 @@ let package = Package(
     name: "DateToolsSwift",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v16),
         .tvOS(.v12),
         .macOS(.v10_13),
     ],
